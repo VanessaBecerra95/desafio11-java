4e89a6edf2a07d944f7f6bdf2c18e57f2f8ae798
VanessaBecerra95 <vanessa.becerra.zamorano@ciisa.cl>
2024-06-07 20:39:37 -0400

    edit metod limpiarPantalla and tiempoEspera

--- src/cl/praxis/recliclaJeans/Utilidad.java
+++ src/cl/praxis/recliclaJeans/Utilidad.java
@@ -2,16 +2,16 @@ package cl.praxis.recliclaJeans;
 
 public class Utilidad {
     public static void limpiarPantalla() {
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 20; i++) {
             System.out.println();
         }
     }
 
     public static void tiempoEspera() {
         try {
-            System.out.println("Abandonando el sistema de productos...");
-            Thread.sleep(2000);
-            System.out.println("Acaba de salir del Sistema");
+            System.out.println("Abandonando el sistema de clientes...");
+            Thread.sleep(1000);
+            System.out.println("Acaba de salir del sistema");
         } catch (Exception e) {
             System.out.println(e);
 
